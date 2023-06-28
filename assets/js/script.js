ef0e29fb45c564713518be2aeeac6752ff0f43b2
EdelCorbett <>
2023-06-28 21:51:20 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :o");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}.The correct answer was ${calculatedAnswer[0]}!`);
-
+         incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -68,12 +69,22 @@ function calculateCorrectAnswer() {
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
 }
-
+/**
+ * get the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+}
+/**
+ * get the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
